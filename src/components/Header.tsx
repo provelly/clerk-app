c320db358a7695ac02e117a762034881fac216de
provelly <hinseong489@gmail.com>
2025-10-18 18:11:47 +0900

    Change Header navigation colors and links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+// src/components/Header.tsx
 import {
   SignedIn,
   SignedOut,
@@ -11,11 +12,11 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div>
-      <nav className="bg-red-900 py-4 px-8">
+      <nav className="bg-blue-800 py-4 px-8">
         <div className="flex items-center justify-between container">
           <div className="flex items-center font-bold">
             <Link href="/">
-              <div className="text-lg text-white">Clerk App</div>
+              <div className="text-lg text-white">portfolio</div>
             </Link>
           </div>
           <div className="flex items-center font-bold">
@@ -34,23 +35,18 @@ export default function Header() {
               >
                 Repos
               </Link>
-              <Link
-                href="/courses"
-                className="text-gray-300 hover:text-white mr-4"
-              >
-                Courses
-              </Link>
+              
               <Link
                 href="/dashboard"
                 className="text-gray-300 hover:text-white mr-4"
               >
-                Dashboard-S
+                contact
               </Link>
               <Link
                 href="/dashboard-c"
                 className="text-gray-300 hover:text-white mr-4"
               >
-                Dashboard-C
+                about-me
               </Link>
               <div className="text-gray-300 hover:text-white mr-4">
                 <UserButton />
