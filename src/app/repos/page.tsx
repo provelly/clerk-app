e8868fb0040d1d4c3b20df13a0d4b3fe66159d50
provelly <hinseong489@gmail.com>
2025-10-18 18:14:40 +0900

    Refactor ReposPage to RepoPage with Suspense

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -1,49 +1,22 @@
-import { Repository } from '@/types/repo'
-import { Token } from '@clerk/nextjs/server'
+import Repo from '@/components/Repo'
+import RepoDirs from '@/components/RepoDirs'
 import Link from 'next/link'
-import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
-
-const username = 'provelly'
-
-export default async function ReposPage() {
-  // 1. SSG: Static Site Generation
-  const response = await fetch(`https://api.github.com/users/${username}/repos`,
-  {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
-    },
-  }
-  )
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  const repos = await response.json()
-  // console.log(repos)
-
+import React, { Suspense } from 'react'
+export default function RepoPage({ params }: { params: { name: string } }) {
   return (
-    <div>
-      <h2 className='text-2xl font-bold mb-4'>
-        Github Repositories of {username}
-      </h2>
-      <ul>
-        {repos.map((repo: Repository) => (
-          <li key={repo.id} className='bg-gray-100 m-4 p-4 rounded-md'>
-            <Link href={`/repos/${repo.name}`}>
-              <h3 className='text-xl font-bold'>{repo.name}</h3>
-              <p>{repo.description}</p>
-              <div className='flex justify-between items-center'>
-                <span className='flex items-center gap-1'>
-                  <FaStar /> {repo.stargazers_count}
-                </span>
-                <span className='flex items-center gap-1'>
-                  <FaCodeBranch /> {repo.forks_count}
-                </span>
-                <span className='flex items-center gap-1'>
-                  <FaEye /> {repo.watchers_count}
-                </span>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col justify-start items-start max-w-lg">
+      <Link
+        href="/repos"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+      >
+        Back to Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={params.name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={params.name} />
+      </Suspense>
     </div>
   )
 }
