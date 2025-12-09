e544a6cf33f101ddf144a95245ae45699066ec4b
provelly <hinseong489@gmail.com>
2025-12-10 00:29:22 +0900

    Rename RepoPage to RepoDetailPage and update layout

--- src/app/repos/[name]/page.tsx
+++ src/app/repos/[name]/page.tsx
@@ -1,22 +1,25 @@
-import Repo from '@/components/Repo'
 import RepoDirs from '@/components/RepoDirs'
 import Link from 'next/link'
 import React, { Suspense } from 'react'
-export default function RepoPage({ params }: { params: { name: string } }) {
+
+export default function RepoDetailPage({ params }: { params: { name: string } }) {
+  const { name } = params;
+
   return (
-    <div className='flex flex-col justify-start items-start max-w-lg'>
+    <div className='flex flex-col justify-start items-start max-w-lg p-4'>
       <Link
         href='/repos'
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'
       >
         Back to Repositories
       </Link>
-      <Suspense fallback={<div>Loading repo...</div>}>
-        <Repo name={params.name} />
-      </Suspense>
-      <Suspense fallback={<div>Loading directories...</div>}>
-        <RepoDirs name={params.name} />
-      </Suspense>
+      
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full">
+        <h2 className="text-2xl font-bold mb-4">Repository: {name}</h2>
+        <Suspense fallback={<div>Loading directories...</div>}>
+          <RepoDirs name={name} />
+        </Suspense>
+      </div>
     </div>
   )
 }
